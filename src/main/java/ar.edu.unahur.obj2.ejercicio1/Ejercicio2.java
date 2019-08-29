62d9b3e895cfdd7e2516069ca1d7101be577b82f
B1max <beunomax@yahoo.com.ar>
2019-08-28 21:24:55 -0300

    ejer2 y 3

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio2.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio2.java
@@ -1,4 +1,23 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
 public class Ejercicio2 {
+    int[] los20 = {6,64,27,45,7,8,9,54,4,3,56,16,27,788,99,0,7655,62,854};
+    int ingreso = 30;
+    public int[] ObtenerMayor (int[] arrr){
+        int[] resultado = new int[20];
+        int indice = 0;
+        int mayor;
+        mayor = arrr[0];
+        int puntero = 0;
+
+        while (puntero < arrr.length) {
+            if (arrr[puntero] > mayor) {
+                resultado[indice] = arrr[puntero];
+                indice++;
+            }
+            puntero++;
+        }
+        return resultado;
+    }
+
 }
--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio3.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio3.java
@@ -1,4 +1,45 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
 public class Ejercicio3 {
+    public static void main (String[]args){
+        double[] temperaturas = {12,13,14,15,16,17,18,19,20,21,22,23,22,21,25,21,20,19,18,17,16,15,14,13};
+        mostrarTemperaturas(temperaturas);
+    }
+
+    public static double menor(double[] arrr){
+        double men = arrr[0];
+        int indice = arrr.length-1;
+        while (indice >= 0){
+            if(arrr[indice] < men){
+                men = arrr[indice];
+            }
+            indice --;
+        }
+        return men;
+    }
+    public static double mayor (double[] arrr){
+        double may = arrr[0];
+        int indice = arrr.length-1;
+        while (indice >0){
+            if(arrr[indice] > may){
+                may = arrr[indice];
+            }
+            indice --;
+        }
+        return may;
+    }
+    public static void mostrarTemperaturas(double[]arrr){
+        for (int i = 0; i <= arrr.length-1;i++){
+            for(int j = 0; j <(int)arrr[i];j++){
+                System.out.print("*");
+            }
+            if(arrr[i]==menor(arrr)){
+                System.out.print("<-Menor");
+            }
+            if(arrr[i]==mayor(arrr)){
+                System.out.print("<-Mayor");
+            }
+            System.out.println();
+        }
+    }
 }
