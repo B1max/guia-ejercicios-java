79ee96db5c4a8abdbf93fdb280e34a774df765e1
B1max <beunomax@yahoo.com.ar>
2019-09-01 19:52:33 -0300

    ejer7 antes del merge

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1s7.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1s7.java
@@ -1,4 +1,33 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
+
+import java.util.Random;
+
 public class Ejercicio1s7 {
+    public static void main(String[]args){
+        mostrarMatriz(cargarMatriz());
+    }
+    private static int[][] cargarMatriz(){
+        int[][] matriz = new int[10][10];
+        for(int i = 0; i<matriz.length; i++){
+            for(int j = 0;j < matriz[0].length;j++){
+                Random r = new Random();
+                int aleatorio = r.nextInt(100);
+                matriz[i][j] = aleatorio;
+            }
+        }
+        return matriz;
+    }
+    private static void mostrarMatriz(int[][] matriz){
+        for(int i = 0; i<matriz.length; i++){
+            for(int j = 0;j < matriz[0].length;j++) {
+                String espacio = "   ";
+                if (matriz[i][j] > 9){
+                    espacio = "  ";
+                }
+                System.out.format("%d"+espacio, matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
